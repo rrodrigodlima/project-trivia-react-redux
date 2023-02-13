0f98174dff333f7a8b7cd994b5cdfce087870f5a
Hector Munhoz <hectormunhozc@gmail.com>
2023-02-13 16:22:33 -0300

    Requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,26 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h2 data-testid="ranking-title">Ranking</h2>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Voltar para o início
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
